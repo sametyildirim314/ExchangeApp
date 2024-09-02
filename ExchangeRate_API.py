9905f3d7f28af98f8eab9b750ebc5339ae84a070
sametyildirim314 <sametyldrmckrxwz@gmail.com>
2024-09-02 13:47:21 +0300

    App language has been changed

--- ExchangeRate_API.py
+++ ExchangeRate_API.py
@@ -5,20 +5,20 @@ api_key="fcc7c87069fd88240d24e4f8"
 
 api_url="https://v6.exchangerate-api.com/v6/fcc7c87069fd88240d24e4f8/latest/"
 
-bozulan_doviz=input("Döviz türü: ")
-bozulan_doviz=bozulan_doviz.upper()
-Alinan_doviz=input("Alinan döviz: ")
-Alinan_doviz=Alinan_doviz.upper()
-miktar=float(input(f"ne kadar {Alinan_doviz} bozdurmak istiyorsunuz: "))
+Currency_to_exchange=input("Currency to exchange: ")
+Currency_to_exchange=Currency_to_exchange.upper()
+Currency_to_receive=input("Currency to receive: ")
+Currency_to_receive=Currency_to_receive.upper()
+amount=float(input(f"How much {Currency_to_exchange} do you want to exchange? "))
 
-sonuc=requests.get(api_url+bozulan_doviz)
+result=requests.get(api_url+Currency_to_exchange)
 
-sonuc=json.loads(sonuc.text)
+result=json.loads(result.text)
 
 
 # for i in sonuc:
 #     print(i)
-x=sonuc["conversion_rates"][Alinan_doviz]
+x=result["conversion_rates"][Currency_to_receive]
 
     
-print(f"{x*miktar:.2f} {Alinan_doviz}")   
+print(f"{x*amount:.2f} {Currency_to_receive}")  
